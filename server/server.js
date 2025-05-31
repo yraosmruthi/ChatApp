622c0ee349424fc6f13be1401c9183c9e967f521
yraosmruthi <yraosmruthi@gmail.com>
2025-05-31 22:52:32 +0530

    made some changes to backend

--- server/server.js
+++ server/server.js
@@ -75,14 +75,6 @@ io.on("connection", (socket) => {
     console.log("User joined room:", room);
   });
 
-  socket.on("typing", (room) => {
-    socket.in(room).emit("typing");
-  });
-
-  socket.on("stop typing", (room) => {
-    socket.in(room).emit("stop typing");
-  });
-
   socket.on("new message", (newMessageRecieved) => {
     let chat = newMessageRecieved.chat;
     if (!chat.users) {
