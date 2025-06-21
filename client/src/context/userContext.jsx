ee43f9b4f457c68af1dbfa7b98ca86cad0622807
yraosmruthi <yraosmruthi@gmail.com>
2025-06-21 13:17:18 +0530

    fixed bug

--- client/src/context/userContext.jsx
+++ client/src/context/userContext.jsx
@@ -34,7 +34,7 @@ const UserProvider = ({ children }) => {
 
   const logout = async () => {
     try {
-      const res = await fetch("http://localhost:3000/api/auth/logout", {
+      const res = await fetch(`${endpoint}api/auth/logout`, {
         method: "POST",
         credentials: "include",
       });
