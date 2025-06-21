169132b5adb3cab9faecbd8de7b752d3596cd15d
yraosmruthi <yraosmruthi@gmail.com>
2025-06-21 13:21:41 +0530

    fixed bug

--- server/server.js
+++ server/server.js
@@ -44,7 +44,7 @@ const io = new Server(server, {
     origin: [
       "http://localhost:5173",
       "http://localhost:3000",
-      "https://chatapp-1-td4o.onrender.com/",
+      "https://chatapp-1-td4o.onrender.com",
       "https://chatapp-67ws.onrender.com",
     ],
     credentials: true,
